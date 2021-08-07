6ee7ea572af80a4a09ff43158d95869718349985
Laura Court <laura.mcourt@gmail.com>
2021-08-07 04:16:57 -0400

    Change random range starting from 1 to 0

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,11 +4,19 @@ class Enigma
 
   def random_num
     5.times.map do
-      rand(1..9)
+      rand(0..9)
     end.join
   end
 
   def todays_date
     Date.today.strftime("%d%m%y")
+  end
+
+  def encrypt(message, key = random_num, date = todays_date)
+    {
+      encryption: message,
+      key: key,
+      date: date
+    }
   end
 end
