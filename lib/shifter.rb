acae52bf2e6440932a69fb8f3f20131a051257ad
Laura Court <laura.mcourt@gmail.com>
2021-08-06 21:06:16 -0400

    Change keys to single each cons method

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -6,27 +6,18 @@ class Shifter
     @date = date
   end
 
-  def a_key
-    a = @key[0..1]
-  end
-
-  def b_key
-    b = @key[1..2]
-  end
-
-  def c_key
-    c = @key[2..3]
-  end
-
-  def d_key
-    d = @key[3..4]
+  def keys
+    a = @key.split("")
+    a.each_cons(2).map do |num|
+      num.join
+    end
   end
 
   def date_squared
     @date.to_i ** 2
   end
 
-  def last_four
-    date_squared.to_s[-4..-1]
+  def offsets
+    date_squared.to_s[-4..-1].split("")
   end
 end
