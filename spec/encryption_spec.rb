fb9f04aa603936f243aa03aec167d8eadd7cd5f8
Laura Court <laura.mcourt@gmail.com>
2021-08-07 21:03:28 -0400

    Test encryption test from shifter class

---
+++ spec/encryption_spec.rb
@@ -0,0 +1,14 @@
+require './lib/encryption'
+require './lib/shifter'
+
+RSpec.describe Encryption do
+  it 'exists' do
+    encrypt = Encryption.new("02715", '040895')
+    expect(encrypt).to be_a(Encryption)
+  end
+
+  it 'encrypts' do
+    encrypt = Encryption.new("02715", '040895')
+    expect(encrypt.encrypt("hello world")).to eq("keder ohulw")
+  end
+end
