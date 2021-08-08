07128c1ad9a13fda87047e5ce9ae8168aa9c288f
Laura Court <laura.mcourt@gmail.com>
2021-08-07 21:04:24 -0400

    Add method for decryption and initialize

---
+++ lib/decryption.rb
@@ -0,0 +1,22 @@
+# require './lib/shifter'
+# class Decryption
+#
+#     include Shifter
+#     attr_reader :key, :date
+#     def initialize(key, date)
+#       @key = key
+#       @date = date
+#     end
+#
+#   def decrypt(message)
+#     message.each_char.with_index.map do |letter, i|
+#       idx = characters.index(letter)
+#       if characters.include?(letter)
+#           a_idx = (idx - shifts.values[i % 4]) % characters.length
+#           characters[a_idx]
+#       else
+#         letter
+#       end
+#     end.join
+#   end
+# end
