394e6d24ece3b8ce0ceab7913305fc3cc5df22b5
Laura Court <laura.mcourt@gmail.com>
2021-08-07 21:06:21 -0400

    Comment in tests

--- spec/decryption_spec.rb
+++ spec/decryption_spec.rb
@@ -1,14 +1,14 @@
-# require './lib/decryption'
-# require './lib/shifter'
-#
-# RSpec.describe Decryption do
-#   it 'exists' do
-#     decrypt = Decryption.new("02715", '040895')
-#     expect(decrypt).to be_a(Decryption)
-#   end
-#
-#   it 'decrypts' do
-#     decrypt = Decryption.new("02715", '040895')
-#     expect(decrypt.decrypt("keder ohulw")).to eq("hello world")
-#   end
-# end
+require './lib/decryption'
+require './lib/shifter'
+
+RSpec.describe Decryption do
+  it 'exists' do
+    decrypt = Decryption.new("02715", '040895')
+    expect(decrypt).to be_a(Decryption)
+  end
+
+  it 'decrypts' do
+    decrypt = Decryption.new("02715", '040895')
+    expect(decrypt.decrypt("keder ohulw")).to eq("hello world")
+  end
+end
