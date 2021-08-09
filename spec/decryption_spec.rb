995ae00dd5472b9ab61c1ba10656d77aa835876a
Laura Court <laura.mcourt@gmail.com>
2021-08-09 04:00:38 -0400

    Comment out everything; not sure if I'm keeping it

--- spec/decryption_spec.rb
+++ spec/decryption_spec.rb
@@ -1,48 +1,48 @@
-require './lib/decryption'
-require './lib/keyable'
-
-RSpec.describe Decryption do
-  it 'exists' do
-    decrypt = Decryption.new("02715", '040895')
-
-    expect(decrypt).to be_a(Decryption)
-  end
-
-  it 'has characters' do
-    decrypt = Decryption.new("02715", '040895')
-
-    expect(decrypt.chars).to eq(("a".."z").to_a << " ")
-  end
-
-  it 'has keys' do
-    decrypt = Decryption.new("02715", '040895')
-
-    expected = ["02", "27", "71", "15"]
-
-    expect(decrypt.keys("02715")).to eq(expected)
-  end
-
-  it 'has date_squared' do
-    decrypt = Decryption.new("02715", '040895')
-
-    expect(decrypt.date_squared('040895')).to eq(1672401025)
-  end
-
-  it 'has offsets' do
-    decrypt = Decryption.new("02715", '040895')
-
-    expect(decrypt.offsets).to eq(["1", "0", "2", "5"])
-  end
-
-  it 'has shifts' do
-    decrypt = Decryption.new("02715", '040895')
-
-    expect(decrypt.shifts).to eq({ :A => 3, :B => 27, :C => 73, :D => 20})
-  end
-
-  it 'decrypts' do
-    decrypt = Decryption.new("02715", '040895')
-    
-    expect(decrypt.decrypt("keder ohulw")).to eq("hello world")
-  end
-end
+# require './lib/decryption'
+# require './lib/keyable'
+#
+# RSpec.describe Decryption do
+#   it 'exists' do
+#     decrypt = Decryption.new("02715", '040895')
+#
+#     expect(decrypt).to be_a(Decryption)
+#   end
+#
+#   it 'has characters' do
+#     decrypt = Decryption.new("02715", '040895')
+#
+#     expect(decrypt.chars).to eq(("a".."z").to_a << " ")
+#   end
+#
+#   it 'has keys' do
+#     decrypt = Decryption.new("02715", '040895')
+#
+#     expected = ["02", "27", "71", "15"]
+#
+#     expect(decrypt.keys("02715")).to eq(expected)
+#   end
+#
+#   it 'has date_squared' do
+#     decrypt = Decryption.new("02715", '040895')
+#
+#     expect(decrypt.date_squared('040895')).to eq(1672401025)
+#   end
+#
+#   it 'has offsets' do
+#     decrypt = Decryption.new("02715", '040895')
+#
+#     expect(decrypt.offsets).to eq(["1", "0", "2", "5"])
+#   end
+#
+#   it 'has shifts' do
+#     decrypt = Decryption.new("02715", '040895')
+#
+#     expect(decrypt.shifts).to eq({ :A => 3, :B => 27, :C => 73, :D => 20})
+#   end
+#
+#   it 'decrypts' do
+#     decrypt = Decryption.new("02715", '040895')
+#
+#     expect(decrypt.decrypt("keder ohulw")).to eq("hello world")
+#   end
+# end
