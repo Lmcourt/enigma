63193a889da9a6782daeaf31b075c70080f080ae
Laura Court <laura.mcourt@gmail.com>
2021-08-09 03:58:17 -0400

    Merged decryption and encryption class back into one class

---
+++ lib/cryption.rb
@@ -0,0 +1,34 @@
+require './lib/keyable'
+class Cryption
+
+  include Keyable
+  attr_reader :key, :date
+  def initialize(key, date)
+    @key = key
+    @date = date
+  end
+
+  def chars
+    ("a".."z").to_a << " "
+  end
+
+  def encrypt(message)
+    message.downcase.each_char.with_index.map do |letter, i|
+      if chars.include?(letter)
+          chars[(chars.index(letter) + shifts.values[i % 4]) % chars.length]
+      else
+        letter
+      end
+    end.join
+  end
+
+  def decrypt(message)
+    message.downcase.each_char.with_index.map do |letter, i|
+      if chars.include?(letter)
+          chars[(chars.index(letter) - shifts.values[i % 4]) % chars.length]
+      else
+        letter
+      end
+    end.join
+  end
+end
